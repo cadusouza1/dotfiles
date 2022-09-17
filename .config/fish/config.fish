8f6ff2c462205081cfc7264029407c2086c70173
CaduSouza1 <cadusouza14@gmail.com>
2022-09-17 18:39:19 -0300

    Moved shell aliases to a separate file

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -38,57 +38,7 @@ setxkbmap -model abnt2 -layout br -variant abnt2
 
 fish_vi_key_bindings
 
-### Aliases ###
-alias e="$EDITOR"
-alias t="tldr"
-alias r="rich"
-
-# Fish config
-alias fconf="e $FISHDOTDIR/config.fish"
-alias fup="source $FISHDOTDIR/config.fish"
-
-alias ls="exa --color=auto -a --group-directories-first"
-alias ll="exa --color=auto --icons -a --group-directories-first"
-
-function lt 
-    if [[ $1 -eq "" ]]; then
-        dept_level = 2
-    else
-        dept_level = $1
-    end
-
-    exa --color=auto --icons -a --group-directories-first -T -R -L $dept_level -l --no-permissions --no-user --no-time --git
-end
-
-## Cargo
-alias cgr="cargo run"
-alias cgb="cargo build"
-alias cgbr="cargo build --release"
-alias cgi="cargo install"
-alias cgu="cargo uninstall"
-alias cga="cargo add"
-alias cgrm="cargo rm"
-alias cgc="cargo clippy"
-
-alias nplug="cd $XDG_DATA_HOME/nvim/site/pack/packer/start"
-alias nconf="cd $XDG_CONFIG_HOME/nvim/lua && e ../init.lua && cd -"
-
-alias pp="source ~/.scripts/pp.fish"
-
-alias sain="sudo apt install"
-alias sarm="sudo apt remove" 
-alias saup="sudo apt update"
-alias saug="sudo apt upgrade"
-alias sapu="sudo apt purge"
-
-function mkcd
-    mkdir $argv[1] 
-    cd $argv[1]
-end
-
-function ch 
-    curl cheat.sh/$argv[1]
-end
+source $FISHDOTDIR/aliases.fish
 
 #set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/work/.ghcup/bin $PATH # ghcup-env
 
