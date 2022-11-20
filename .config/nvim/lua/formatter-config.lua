f34a2abdab7c6661ef9fcf8b7e789a8d61a2c084
CaduSouza1 <cadusouza14@gmail.com>
2022-11-19 22:31:26 -0300

    Added new formatters

--- .config/nvim/lua/formatter-config.lua
+++ .config/nvim/lua/formatter-config.lua
@@ -6,10 +6,11 @@ require("formatter").setup({
 		go = { require("formatter.filetypes.go").gofmt },
 		haskell = { require("formatter.filetypes.haskell").stylish_haskell },
 		javascript = { require("formatter.filetypes.javascript").jsbeatify },
+		sh = { require("formatter.filetypes.sh").shfmt },
+		c = { require("formatter.filetypes.c").clangformat },
+		yaml = { require("formatter.filetypes.yaml").prettier },
 	},
 })
-
-vim.keymap.set("n", "<leader>fo", "<cmd>Format<cr>")
 
 local group = vim.api.nvim_create_augroup("FormatterGroup", { clear = true })
 vim.api.nvim_create_autocmd("BufWritePost", { command = "FormatWrite", group = group })
