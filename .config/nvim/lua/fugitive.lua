40128a585a4f2a63112393097bf8e435b09b1e97
CaduSouza1 <cadusouza14@gmail.com>
2022-11-19 22:32:12 -0300

    Simplified some keys and removed a duplicate key definition

--- .config/nvim/lua/fugitive.lua
+++ .config/nvim/lua/fugitive.lua
@@ -1,22 +1,12 @@
 local git_keymaps = {
 	["a"] = ":G add %",
-	["di"] = ":G diff %",
-	["da"] = ":G diff",
-	["dp"] = ":Ghdiffsplit",
-	["ds"] = ":Gvdiffsplit",
-	["dt"] = ":G difftool",
-	["ed"] = ":Gedit",
-	["c"] = ":G commit",
-	["r"] = ":G rebase -i",
+	["d"] = ":G diff %",
+	["D"] = ":G diff",
 	["l"] = ":G log",
-	["b"] = ":G blame",
-	["m"] = ":G mergetool",
-	["s"] = ":G ",
+	["r"] = ":G rebase -i",
+	["s"] = ":G status",
 }
 
 for key, map in pairs(git_keymaps) do
 	vim.keymap.set("n", "<leader>g" .. key, map .. "<cr>")
 end
-
-vim.keymap.set("n", "<leader>gad", ":G add ")
-vim.keymap.set("n", "<leader>gpu", ":G push ")
