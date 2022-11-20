4b5442ade23330762049c4c9b8621ab75bba127e
CaduSouza1 <cadusouza14@gmail.com>
2022-11-19 22:49:15 -0300

    Added so much I dont wanna write it all

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,9 +1,3 @@
--- xmonad example config file.
---
--- A template showing all available configuration hooks,
--- and how to override the defaults in your own xmonad.hs conf file.
---
--- Normally, you'd only override those defaults you care about.
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Use head" #-}
 {-# HLINT ignore "Use void" #-}
@@ -15,16 +9,12 @@ import           System.Process
 import           Text.Show
 
 import           XMonad
-import           XMonad.Actions.CycleRecentWS
-import           XMonad.Actions.DynamicProjects
-import           XMonad.Actions.EasyMotion
+import qualified XMonad.Actions.FlexibleResize  as Flex
 import           XMonad.Actions.RepeatAction
 import           XMonad.Actions.RotSlaves
-import           XMonad.Actions.Search
 import           XMonad.Actions.SpawnOn
 import           XMonad.Actions.Volume
 import           XMonad.Actions.WithAll
-import           XMonad.Hooks.WindowSwallowing
 
 import           XMonad.Hooks.DynamicLog
 import           XMonad.Hooks.EwmhDesktops
@@ -35,14 +25,11 @@ import           XMonad.Layout.NoBorders
 import           XMonad.Layout.Renamed
 import           XMonad.Layout.ResizableTile
 import           XMonad.Layout.Spacing
-import           XMonad.Layout.SubLayouts
 import           XMonad.Layout.ThreeColumns
-import           XMonad.Layout.TwoPane
+import           XMonad.Layout.ToggleLayouts
 import           XMonad.Layout.WindowNavigation
-import           XMonad.Layout.WorkspaceDir
 
 import           XMonad.Util.EZConfig
-import           XMonad.Util.Loggers
 import           XMonad.Util.Run
 import           XMonad.Util.SpawnOnce
 import           XMonad.Util.Spotify
@@ -51,11 +38,9 @@ import           XMonad.Prompt
 
 import qualified Data.Binary                    as GHC
 import qualified Data.Binary                    as GHC.Word
-import qualified Data.Map                       as M
-import           Data.Maybe
+-- import qualified Data.Map                       as M
+-- import           Data.Maybe
 import qualified Foreign.C
-import qualified XMonad.Actions.Search          as S
-import qualified XMonad.Actions.Submap          as SM
 import qualified XMonad.StackSet                as W
 
 import           Colors.Dracula
@@ -64,7 +49,7 @@ myFont :: String -> GHC.Word.Word32 -> String
 myFont weight size = "xft:FiraCode Nerd Font:weight=" ++ weight ++ ":pixelsize=" ++ show size
 
 myTerminal :: String
-myTerminal = "alacritty"
+myTerminal = "alacritty msg create-window || alacritty"
 
 myBrowser :: String
 myBrowser = "firefox"
@@ -112,28 +97,19 @@ myNormalBorderColor = "#000000"
 -- Border colors for focused window
 myFocusedBorderColor:: String
 myFocusedBorderColor = "#008b8b"
---myFocusedBorderColor = "#161925"
 
 recompileAndRestartXMonad :: String
 recompileAndRestartXMonad = "notify-send 'Recompiling XMonad' -t 1000 & killall xmobar & xmonad --recompile && xmonad --restart && notify-send 'Recompilation successfully' -t 1000"
 
-projects :: [Project]
-projects = []
-
 myXPConfig = def {
-      font         = myFont "regular" 24
+      font         = myFont "regular" 16
     , bgColor      = colorBack
     , fgColor      = colorFore
     , bgHLight     = color06
     , fgHLight     = color01
+    , position     = Top
     , height       = 24
     , autoComplete = Just 100000
-    }
-
-myEMConfig = def {
-      sKeys  = AnyKeys [xK_s, xK_f, xK_j, xK_k]
-    , emFont = myFont "regular" 32
-    , overlayF = textSize
     }
 
 notify :: Int -> String -> X ()
@@ -145,16 +121,20 @@ toggleBorders = do
     sendMessage (ModifyWindowBorderEnabled not)
     sendMessage ToggleStruts
 
+-- Maintain only one alacritty process
+myRunInTerm :: String -> X ()
+myRunInTerm cmd = spawn $ "alacritty msg create-window -e " ++ cmd ++ " || alacritty -e " ++ cmd
+
 edit :: String -> X ()
-edit filePath = runInTerm "" $ myEditor ++ " " ++ filePath
+edit filePath = myRunInTerm $ myEditor ++ " " ++ filePath
 
 fuzzyEdit :: String -> X ()
-fuzzyEdit path = runInTerm "" $ "~/.scripts/fzf/fuzzy-edit.sh " ++ path
+fuzzyEdit path = myRunInTerm $ "fuzzy.sh edit " ++ path
 
 notifyAndSpawn :: String -> X ()
 notifyAndSpawn command = do
     notify 1 $ "Starting " ++ command
-    spawn command
+    spawnHere command
 
 bluetoothConnect :: String -> String -> X ()
 bluetoothConnect deviceID deviceName = do
@@ -173,44 +153,45 @@ notifySetVolume volume = do
     -- The volume is extremelly close to the actual volume that was set, so it is not a problem to round it
     notify 1 $ "Volume: " ++ show (round v)
 
-viewWS ws i = windows $ W.greedyView $ ws !! i
+viewWS :: Int -> X ()
+viewWS i = windows $ W.greedyView $ myWorkspaces !! i
 
-moveToWS ws i = windows $ W.shift $ ws !! i
+moveToWS :: Int -> X ()
+moveToWS i = windows $ W.shift $ myWorkspaces !! i
 
-moveAndViewWS ws i = moveToWS ws i >> viewWS ws i
+moveAndViewWS :: Int -> X ()
+moveAndViewWS i = moveToWS i >> viewWS i
 
 myKeys :: [(String, X ())]
 myKeys = [
     -- launch a terminal
       ("M-<Return>", spawn myTerminal)
 
-    -- Quickly create and setup projects
-    , ("M-/",         shiftToProjectPrompt myXPConfig)
-    , ("M-S-<Space>", switchProjectPrompt myXPConfig)
+    , ("M-m", myRunInTerm "fuzzy.sh manpage")
 
-    -- Change the default directory in a workspace
-    , ("M-x", changeDir myXPConfig)
-
-    , ("M-f", selectWindow myEMConfig >>= (`whenJust` windows . W.focusWindow))
-    , ("M-t", toggleRecentWS)
+    -- Make the windows fullscreen
+    , ("M-f", sendMessage ToggleLayout)
 
     -- Aplications to open
     , ("M-o b", notifyAndSpawn myBrowser)
     , ("M-o m", notifyAndSpawn myMusicPlayer)
     , ("M-o d", notifyAndSpawn "discord")
-    , ("M-o h", runInTerm "" "htop")
-    , ("M-o l", runInTerm "" "~/.scripts/fzf/urls/fuzzy-url.sh")
+
+    -- Terminal commands
+    , ("M-o h", myRunInTerm "htop")
+    , ("M-o c", myRunInTerm "fuzzy.sh cheatsheet")
+    , ("M-o l", spawn "fuzzy.sh urls")
     , ("M-o s", spawn "xfce4-screenshooter")
 
-    -- Bluetooth devices I use
-    , ("M-b c p", bluetoothConnect    "A4:77:58:79:9E:2F" "Philips SHB3175")
-    , ("M-b d p", bluetoothDisconnect "A4:77:58:79:9E:2F" "Philips SHB3175")
-    , ("M-b c r", bluetoothConnect    "1C:52:16:87:7B:D6" "Redmi Airdots S")
-    , ("M-b d r", bluetoothDisconnect "1C:52:16:87:7B:D6" "Redmi Airdots S")
-    , ("M-b c b", bluetoothConnect    "16:48:75:47:EF:3D" "BT SPEAKER")
-    , ("M-b d b", bluetoothDisconnect "16:48:75:47:EF:3D" "BT SPEAKER")
-    , ("M-b c e", bluetoothConnect    "FC:58:FA:73:76:2A" "887")
-    , ("M-b d e", bluetoothDisconnect "FC:58:FA:73:76:2A" "887")
+    -- Bluetooth devices that I use
+    , ("M-b c p", spawn "bluetooth.sh connect 10 Philips-SHB3175 A4:77:58:79:9E:2F")
+    , ("M-b d p", spawn "bluetooth.sh disconnect Philips-SHB3175 A4:77:58:79:9E:2F")
+    , ("M-b c r", spawn "bluetooth.sh connect 10 Redmi-Airdots-S 1C:52:16:87:7B:D6")
+    , ("M-b d r", spawn "bluetooth.sh disconnect Redmi-Airdots-S 1C:52:16:87:7B:D6")
+    , ("M-b c b", spawn "bluetooth.sh connect 10 BT-SPEAKER 16:48:75:47:EF:3D")
+    , ("M-b d b", spawn "bluetooth.sh disconnect BT-SPEAKER 16:48:75:47:EF:3D")
+    , ("M-b c e", spawn "bluetooth.sh connect 10 887 FC:58:FA:73:76:2A")
+    , ("M-b d e", spawn "bluetooth.sh disconnect 887 FC:58:FA:73:76:2A")
 
     -- Common files thay I edit
     , ("M-e w", edit "~/.xmonad/xmonad.hs")
@@ -223,6 +204,91 @@ myKeys = [
     , ("M-e n", fuzzyEdit "~/.config/nvim/")
     , ("M-e c", fuzzyEdit "~/.config/ ~/.scripts/ ~/.xmonad/ ~/.local/bin/")
     , ("M-e p", fuzzyEdit "~/.local/share/nvim/site/pack/packer/start/")
+
+    -- Operations with the master window
+    , ("M-S-m", windows W.swapMaster) -- Swap the focused window and the master window
+    , ("M-S-,", sendMessage $ IncMasterN 1) -- Increment the number of windows in the master area
+    , ("M-S-.", sendMessage $ IncMasterN $ -1) -- Decrease the number of windows in the master area
+    -- , ("M-m",   windows W.focusMaster) -- Move focus to the master window
+
+    -- Operations with windows
+    , ("M-j",   windows W.focusDown)
+    , ("M-k",   windows W.focusUp)
+    , ("M-h",   sendMessage $ Go L)
+    , ("M-l",   sendMessage $ Go R)
+    , ("M-S-j", windows W.swapDown)
+    , ("M-S-k", windows W.swapUp)
+    , ("M-S-h", sendMessage $ Swap L)
+    , ("M-S-l", sendMessage $ Swap R)
+    , ("M-S-t", withFocused $ windows . W.sink) -- Push window back into tiling
+    , ("M-C-j", rotSlavesUp)
+    , ("M-C-k", rotSlavesDown)
+    , ("M-<Space>", sendMessage NextLayout)   -- Rotate through the available layout algorithms
+
+    , ("M-C-h", sendMessage Shrink) -- Shrink the master area
+    , ("M-C-l", sendMessage Expand) -- Expand the master area
+    , ("M-C-a", sendMessage MirrorShrink) -- Shrink the other windows
+    , ("M-C-z", sendMessage MirrorExpand) -- Expand the other windows
+
+    , ("M-S-q",  kill) -- close focused window
+    , ("M-C-q",  killAll) -- close all windows
+    , ("M-M1-q", killOthers) -- close all windows without focus
+
+    , ("M-p",   spawn "rofi -show run")
+    , ("M-q",   spawn recompileAndRestartXMonad)
+    , ("M-S-c", io exitSuccess) -- Quit xmonad
+
+    -- TMUX session manager
+    , ("M-S-s", myRunInTerm "tms")
+
+    -- Spotify integration
+    , ("M-s j", audioPrev)
+    , ("M-s k", audioNext)
+    , ("M-s p", audioPlayPause)
+
+    -- Switch and move between workspaces
+    , ("M-1",   viewWS        0)
+    , ("M-2",   viewWS        1)
+    , ("M-3",   viewWS        2)
+    , ("M-4",   viewWS        3)
+    , ("M-5",   viewWS        4)
+    , ("M-6",   viewWS        5)
+    , ("M-7",   viewWS        6)
+    , ("M-8",   viewWS        7)
+    , ("M-9",   viewWS        8)
+    , ("M-S-1", moveToWS      0)
+    , ("M-S-2", moveToWS      1)
+    , ("M-S-3", moveToWS      2)
+    , ("M-S-4", moveToWS      3)
+    , ("M-S-5", moveToWS      4)
+    , ("M-S-6", moveToWS      5)
+    , ("M-S-7", moveToWS      6)
+    , ("M-S-8", moveToWS      7)
+    , ("M-S-9", moveToWS      8)
+    , ("M-C-1", moveAndViewWS 0)
+    , ("M-C-2", moveAndViewWS 1)
+    , ("M-C-3", moveAndViewWS 2)
+    , ("M-C-4", moveAndViewWS 3)
+    , ("M-C-5", moveAndViewWS 4)
+    , ("M-C-6", moveAndViewWS 5)
+    , ("M-C-7", moveAndViewWS 6)
+    , ("M-C-8", moveAndViewWS 7)
+    , ("M-C-9", moveAndViewWS 8)
+
+    -- Volume control
+    , ("M-<F1>", raiseVolume 5 >>= \x -> notify 1 $ show x)
+    , ("M-<F2>", lowerVolume 5 >>= \x -> notify 1 $ show x)
+    , ("M-<F3>", toggleMute >> return ())
+    , ("M-v 1",  notifySetVolume 10 )
+    , ("M-v 2",  notifySetVolume 20 )
+    , ("M-v 3",  notifySetVolume 30 )
+    , ("M-v 4",  notifySetVolume 40 )
+    , ("M-v 5",  notifySetVolume 50 )
+    , ("M-v 6",  notifySetVolume 60 )
+    , ("M-v 7",  notifySetVolume 70 )
+    , ("M-v 8",  notifySetVolume 80 )
+    , ("M-v 9",  notifySetVolume 90 )
+    , ("M-v 0",  notifySetVolume 100)
 
     -- Border keybindings
     , ("M-=",   incScreenWindowSpacing  1)
@@ -239,106 +305,30 @@ myKeys = [
     , ("M-r 9", setScreenWindowSpacing 90)
     , ("M-r t", toggleBorders)
 
-    -- Operations with the master window
-    , ("M-S-m", windows W.swapMaster) -- Swap the focused window and the master window
-    , ("M-S-,", sendMessage $ IncMasterN 1) -- Increment the number of windows in the master area
-    , ("M-S-.", sendMessage $ IncMasterN $ -1) -- Decrease the number of windows in the master area
-    , ("M-m",   windows W.focusMaster) -- Move focus to the master window
-
-    -- Operations with windows
-    , ("M-j",   windows W.focusDown)
-    , ("M-k",   windows W.focusUp)
-    , ("M-h",   sendMessage $ Go L)
-    , ("M-l",   sendMessage $ Go R)
-    , ("M-S-j", windows W.swapDown)
-    , ("M-S-k", windows W.swapUp)
-    , ("M-S-h", sendMessage $ Swap L)
-    , ("M-S-l", sendMessage $ Swap R)
-    , ("M-S-t", withFocused $ windows . W.sink) -- Push window back into tiling
-    , ("M-<Tab>",   rotSlavesDown)
-    , ("M-S-<Tab>", rotSlavesUp)
-    , ("M-<Space>", sendMessage NextLayout)   -- Rotate through the available layout algorithms
-
-    , ("M-C-h", sendMessage Shrink) -- Shrink the master area
-    , ("M-C-l", sendMessage Expand) -- Expand the master area
-    , ("M-C-a", sendMessage MirrorShrink) -- Shrink the other windows
-    , ("M-C-z", sendMessage MirrorExpand) -- Expand the other windows
-
-    , ("M-S-q",  kill) -- close focused window
-    , ("M-C-q",  killAll)
-    , ("M-M1-q", killOthers)
-
-    , ("M-p",   spawn "rofi -show run")
-    , ("M-q",   spawn recompileAndRestartXMonad)
-    , ("M-S-c", io exitSuccess) -- Quit xmonad
-
-    -- TMUX sessions
-    , ("M-S-s", runInTerm "" "tms")
-    , ("M-s p", runInTerm "" "tms polynomial-calculus")
-    , ("M-s i", runInTerm "" "tms image-duplicate-detector")
-    , ("M-s y", runInTerm "" "tms yew-app")
-
-    -- Spotify integration
-    , ("M-s j", audioPrev)
-    , ("M-s k", audioNext)
-    , ("M-s p", audioPlayPause)
-
-    -- Switch and move between workspaces
-    , ("M-1",   viewWS        myWorkspaces 0)
-    , ("M-2",   viewWS        myWorkspaces 1)
-    , ("M-3",   viewWS        myWorkspaces 2)
-    , ("M-4",   viewWS        myWorkspaces 3)
-    , ("M-5",   viewWS        myWorkspaces 4)
-    , ("M-6",   viewWS        myWorkspaces 5)
-    , ("M-7",   viewWS        myWorkspaces 6)
-    , ("M-8",   viewWS        myWorkspaces 7)
-    , ("M-9",   viewWS        myWorkspaces 8)
-    , ("M-S-1", moveToWS      myWorkspaces 0)
-    , ("M-S-2", moveToWS      myWorkspaces 1)
-    , ("M-S-3", moveToWS      myWorkspaces 2)
-    , ("M-S-4", moveToWS      myWorkspaces 3)
-    , ("M-S-5", moveToWS      myWorkspaces 4)
-    , ("M-S-6", moveToWS      myWorkspaces 5)
-    , ("M-S-7", moveToWS      myWorkspaces 6)
-    , ("M-S-8", moveToWS      myWorkspaces 7)
-    , ("M-S-9", moveToWS      myWorkspaces 8)
-    , ("M-C-1", moveAndViewWS myWorkspaces 0)
-    , ("M-C-2", moveAndViewWS myWorkspaces 1)
-    , ("M-C-3", moveAndViewWS myWorkspaces 2)
-    , ("M-C-4", moveAndViewWS myWorkspaces 3)
-    , ("M-C-5", moveAndViewWS myWorkspaces 4)
-    , ("M-C-6", moveAndViewWS myWorkspaces 5)
-    , ("M-C-7", moveAndViewWS myWorkspaces 6)
-    , ("M-C-8", moveAndViewWS myWorkspaces 7)
-    , ("M-C-9", moveAndViewWS myWorkspaces 8)
-
-    -- Volume control
-    , ("M-<F1>", raiseVolume 5 >>= \x -> notify 1 $ show x)
-    , ("M-<F2>", lowerVolume 5 >>= \x -> notify 1 $ show x)
-    , ("M-<F3>", toggleMute >> return ())
-    , ("M-v 1",  notifySetVolume 10 )
-    , ("M-v 2",  notifySetVolume 20 )
-    , ("M-v 3",  notifySetVolume 30 )
-    , ("M-v 4",  notifySetVolume 40 )
-    , ("M-v 5",  notifySetVolume 50 )
-    , ("M-v 6",  notifySetVolume 60 )
-    , ("M-v 7",  notifySetVolume 70 )
-    , ("M-v 8",  notifySetVolume 80 )
-    , ("M-v 9",  notifySetVolume 90 )
-    , ("M-v 0",  notifySetVolume 100)
+    -- Resize focused window with the mouse
+    , ("M-<button3>", withFocused Flex.mouseResizeWindow)
     ]
 
 tall =
       renamed [Replace "Tall"]
-    $ spacingRaw False (Border 10 10 10 10) True (Border 10 10 10 10) True
+    $ spacingRaw False (Border 5 5 5 5) True (Border 5 5 5 5) True
     $ smartBorders
     $ avoidStruts
     $ windowNavigation
     $ ResizableTall 1 (2/100) (1/2) []
 
+mirrorTall =
+      renamed [Replace "Mirror Tall"]
+    $ spacingRaw False (Border 5 5 5 5) True (Border 5 5 5 5) True
+    $ smartBorders
+    $ avoidStruts
+    $ windowNavigation
+    $ Mirror
+    $ ResizableTall 2 (2/100) (1/2) []
+
 tallMasterFocus =
-      renamed [Replace "TallMasterFocus"]
-    $ spacingRaw False (Border 10 10 10 10) True (Border 10 10 10 10) True
+      renamed [Replace "Tall Master Focus"]
+    $ spacingRaw False (Border 5 5 5 5) True (Border 5 5 5 5) True
     $ smartBorders
     $ avoidStruts
     $ windowNavigation
@@ -346,28 +336,26 @@ tallMasterFocus =
 
 threeColumns =
       renamed [Replace "ThreeCol"]
-    $ spacingRaw False (Border 10 10 10 10) True (Border 10 10 10 10) True
+    $ spacingRaw False (Border 5 5 5 5) True (Border 5 5 5 5) True
     $ smartBorders
     $ avoidStruts
     $ windowNavigation
     $ ThreeCol 1 (2/100) (1/2)
 
-full =
-      renamed [Replace "Full"]
-    $ spacingRaw False (Border 10 10 10 10) True (Border 10 10 10 10) True
-    $ avoidStruts
-    $ windowNavigation
-    $ noBorders Full
-
 dwindle =
       renamed [Replace "Dwindle"]
-    $ spacingRaw False (Border 10 10 10 10) True (Border 10 10 10 10) True
+    $ spacingRaw False (Border 5 5 5 5) True (Border 5 5 5 5) True
     $ smartBorders
     $ avoidStruts
     $ windowNavigation
     $ Dwindle R CW 1 1
 
-myLayout = workspaceDir "~" (dwindle ||| tall ||| tallMasterFocus ||| threeColumns ||| full)
+full =
+      renamed [Replace "Full"]
+    $ windowNavigation
+    $ noBorders Full
+
+myLayout = toggleLayouts full (dwindle ||| mirrorTall ||| tall ||| tallMasterFocus ||| threeColumns ||| full)
 
 ------------------------------------------------------------------------
 -- Window rules:
@@ -435,7 +423,7 @@ myStartupHook = do
 
 main = do
     xmproc <- spawnPipe "xmobar -x 0 /home/work/.config/xmobar/xmobar.config"
-    xmonad $ dynamicProjects projects $ docks $ ewmh $ def {
+    xmonad $ docks $ ewmh $ def {
          terminal           = myTerminal
        , focusFollowsMouse  = myFocusFollowsMouse
        , clickJustFocuses   = myClickJustFocuses
