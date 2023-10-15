70bb00bb2e46d9ca0f2ab4a751e2ee09ce1a9040
Cadu Souza <cadusouza14@gmail.com>
2023-10-15 19:13:29 -0300

    formmatting and added path completion

--- .config/nvim/after/plugin/completion.lua
+++ .config/nvim/after/plugin/completion.lua
@@ -13,7 +13,10 @@ cmp.setup({
 		["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
 		["<C-leader>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
 		["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-		["<C-e>"] = cmp.mapping({ i = cmp.mapping.abort(), c = cmp.mapping.close() }),
+		["<C-e>"] = cmp.mapping({
+			i = cmp.mapping.abort(),
+			c = cmp.mapping.close(),
+		}),
 		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 		["<tab>"] = cmp.mapping.select_next_item(),
 		["<S-tab>"] = cmp.mapping.select_prev_item(),
@@ -25,6 +28,7 @@ cmp.setup({
 		{ name = "nvim_lua" },
 	}, {
 		{ name = "buffer" },
+		{ name = "path" },
 	}),
 	experimental = {
 		native_menu = false,
