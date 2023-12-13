4f6b54e75a9fd28bada4b2bbfe521ae38af59352
Cadu Souza <cadusouza14@gmail.com>
2023-12-13 17:11:17 -0300

    I don't really know what changed here, probably a newline got removed

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -34,6 +34,8 @@ export FZF_DEFAULT_COMMAND="fdfind -H . $HOME"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_ALT_C_COMMAND="fdfind -H -t d . $HOME"
 
-set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/work/.ghcup/bin $PATH # ghcup-env
+#set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/work/.ghcup/bin $PATH # ghcup-env
 
 eval (tmuxifier init - fish)
+
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/kdu/.ghcup/bin $PATH # ghcup-env
