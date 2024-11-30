107f8fdf6b989fbebb6f388c4633edf34eb36ee6
Cadu Souza <cadusouza14@gmail.com>
2024-11-30 10:03:48 -0300

    Adapted some keys to work with zsh and changed the fish syntax I forgot to change

--- .config/zsh/custom/keybindings.zsh
+++ .config/zsh/custom/keybindings.zsh
@@ -8,29 +8,29 @@ fuzzy-cd() {
     # commandline -f repaint
 }
 
-set bindings \
-    "jw:$EDITOR    ~/.xmonad/xmonad.hs" \
-    "jt:$EDITOR    ~/.config/tmux/tmux.conf" \
-    "jn:fuzzy-edit ~/.config/nvim/" \
-    "js:fuzzy-edit ~/.scripts/" \
-    "jf:fuzzy-edit ~/.config/fish/" \
-    "jc:fuzzy-edit ~/.config/ ~/.scripts/ ~/.xmonad/ ~/.local/bin/" \
-    "jp:fuzzy-edit ~/.local/share/nvim/site/pack/packer/start/" \
-    "ks:fuzzy-cd   ~/.scripts/" \
-    "kc:fuzzy-cd   ~/.config/ ~/.scripts/ ~/.xmonad/ ~/.local/" \
-    "kp:fuzzy-cd   ~/.local/share/nvim/site/pack/packer/start/" \
-    "u:history-search-backward" \
-    "d:history-search-forward" \
-    "l:accept-autosuggestion" \
-    "a:la && commandline -f repaint" \
-    "w:forward-word" \
-    "W:forward-bigword" \
-    "e:commandline-edit"
+bindings=(
+    "jw:$EDITOR ~/.xmonad/xmonad.hs" 
+    "jt:$EDITOR ~/.config/tmux/tmux.conf" 
+    "jn:fuzzy-edit ~/.config/nvim/" 
+    "js:fuzzy-edit ~/.scripts/" 
+    "jf:fuzzy-edit ~/.config/fish/" 
+    "jc:fuzzy-edit ~/.config/ ~/.scripts/ ~/.xmonad/ ~/.local/bin/" 
+    "jp:fuzzy-edit ~/.local/share/nvim/site/pack/packer/start/" 
+    "ks:fuzzy-cd ~/.scripts/" 
+    "kc:fuzzy-cd ~/.config/ ~/.scripts/ ~/.xmonad/ ~/.local/" 
+    "kp:fuzzy-cd ~/.local/share/nvim/site/pack/packer/start/" 
+    "u:history-search-backward" 
+    "d:history-search-forward" 
+    "l:accept-autosuggestion" 
+    "a:la && commandline -f repaint" 
+    "w:forward-word" 
+    "W:forward-bigword" 
+    "e:edit-command-line")
 
 for binding in $bindings; do
-    set key (echo $binding | cut --delimiter=":" -f1)
-    set fn (echo $binding | cut --delimiter=":" -f2)
+    key=$(cut --delimiter=":" -f1 <<< $binding)
+    fn=$(cut --delimiter=":" -f2 <<< $binding)
 
-    bind -M insert \e$key $fn
+    bindkey "\e$key" $fn
 done
 
